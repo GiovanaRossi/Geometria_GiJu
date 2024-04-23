db666df7bb039ff2bc5a3cff8a5f251a89698f10
Júlia Borges <106270731+JuliaWainer@users.noreply.github.com>
2024-04-23 11:30:59 -0300

    oi

--- Espacial.java
+++ Espacial.java
@@ -1,3 +1,36 @@
+import static java.lang.Math.sqrt;
+
 public class Espacial {
+
     
+    public static void Cone(){
+    float r_co;
+    float g_co;
+    float h_co;
+    double al_co = 2 * Math.PI * r_co * h_co;
+    double ab_co = Math.PI * Math.pow(this.r_co, 2);
+    double v_co = Math.PI * Math.pow(r_co, 2) * h_co;
+
+
+
+    }
+    public static void Cilindro(){
+
+    }
+    public static void Esfera(){
+
+    }
+    public static void Piramide(){
+
+    }
+    public static void Prisma(){
+
+    }
+    public static void Troncos(){
+
+    }
+
+    public static void main(String[] args) {
+        
+    }
 }
