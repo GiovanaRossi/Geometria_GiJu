3e5bddf883133b69fbd0adfeec711d345f5b3c88
Giovana Rossi <giovanarossi.gi@gmail.com>
2024-05-29 10:46:12 -0300

    Tirei umas coisas
    Os extends só bugam o código.

--- GeomeEspacial.java
+++ GeomeEspacial.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
  
-public class GeomeEspacial extends Plana{
+public class GeomeEspacial{
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
        
